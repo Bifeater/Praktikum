d7c136a89a94e1db54888c951fc1539946b6c664
Philipp <mayr-philipp@gmx.de>
2016-10-14 20:09:11 +0200

    test package

---
+++ src/Test/Test.java
@@ -0,0 +1,10 @@
+package Test;
+
+/**
+ * Created by Philipp on 14.10.2016.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("package test hier");
+    }
+}
