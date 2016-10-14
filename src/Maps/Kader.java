d6df0b990689422fc27d33c5a32033c5177d0f26
Philipp <mayr-philipp@gmx.de>
2016-10-14 20:38:38 +0200

    test package

---
+++ src/Maps/Kader.java
@@ -0,0 +1,18 @@
+package Maps;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by Philipp on 14.10.2016.
+ */
+public class Kader {
+
+    private Map<String, Integer> kader = new TreeMap<>();
+    public static void main(String[] args) {
+
+    }
+    
+
+
+}
