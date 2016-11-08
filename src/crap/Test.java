cff788abfba86242af6800e5a82fde521197ed12
Philipp <mayr-philipp@gmx.de>
2016-11-08 22:10:20 +0100

    ins test package kommen jetzzt die tests... :D

--- src/crap/Test.java
+++ src/crap/Test.java
@@ -1,10 +1,10 @@
-package test;
+package crap;
 
 /**
  * Created by Philipp on 14.10.2016.
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("package test hier");
+        System.out.println("package crap hier");
     }
 }
